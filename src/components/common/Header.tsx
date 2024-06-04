6508f4fd14093f383511f7f4b38a6ba054277899
shgusgh12 <https://github.com/shgusgh12>
2024-06-04 15:50:36 +0900

    [feat] : Header, SideBar, Layout 마크업 및 스타일링

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { FaBell } from "react-icons/fa6";
+import { FaCircleUser } from "react-icons/fa6";
+import { FaCircle } from "react-icons/fa6";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const moveToHome = () => {
+    navigate('/')
+  }
+  const moveToMyPage = () => {
+    navigate('/mypage')
+  }
+
+  //zustand로 옮겨서 관리하기
+  const [newNotice, setNewNotice] = useState(true);
+  const [userName, setUserName] = useState('김간병')
+
+  
+  const checkNotice = () => {
+    setNewNotice(false);
+    //모달창 나오게
+  }
+
+  
+ 
   return (
-    <div>
-      header
+    <div className='flex justify-center'>
+      <header className='w-full h-[80px] justify-between flex items-center border-b-[2px] py-[20px] px-[36px] min-w-[1440px]'>
+      <button onClick={moveToHome}>
+        <img className='h-[50px]' src='public/assets/logo-main.png'></img>
+      </button>
+      <div className='flex items-center h-full gap-[52px]'>
+        <button className='relative cursor-pointer' onClick={checkNotice}>
+          <FaBell className='w-[30px] h-[30px]' color='#4166F5'/>
+          {
+            newNotice &&  <FaCircle className='absolute w-[15px] h-[15px] top-0 right-0' color='red'/>
+          }
+        </button>
+        <div className='flex gap-[10px]  w-full items-center'>
+          <button onClick={moveToMyPage}>
+            <FaCircleUser className='w-[30px] h-[30px]' color='#4166F5' />
+          </button>
+          <p className='text-text-medium'>{userName}님 안녕하세요</p>
+        </div>
+      </div>
+      </header>
     </div>
+    
   )
 }
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,15 +2,20 @@ import React, { ReactNode } from 'react'
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 import { Outlet } from 'react-router-dom';
+import SideBar from '../common/SideBar/SideBar';
+import Home from '../../pages/Home';
 
 
 const Layout = () => {
   return (
-    <>
+    <div className='min-w-[1440px] flex justify-center flex-col items-center'>
       <Header/>
-      <Outlet/>
+      <div className='flex'> 
+        <SideBar/>
+        <Outlet/>
+      </div>
       <Footer/>
-    </>
+    </div>
   )
 }
 
