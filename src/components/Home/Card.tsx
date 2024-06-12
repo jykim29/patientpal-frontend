6135638f8bd89c815d493cf2e86034cc8022a21c
shgusgh12 <https://github.com/shgusgh12>
2024-06-12 15:38:17 +0900

    [refactor] : 디자인 수정 및 prettier 적용

--- src/components/Home/Card.tsx
+++ src/components/Home/Card.tsx
@@ -1,89 +1,90 @@
-import { CiCircleChevRight } from "react-icons/ci";
-import { useNavigate } from "react-router-dom";
-import { FaCircleUser } from "react-icons/fa6";
-import { FaCakeCandles } from "react-icons/fa6";
+import { CiCircleChevRight } from 'react-icons/ci';
+import { useNavigate } from 'react-router-dom';
+import { FaCircleUser } from 'react-icons/fa6';
+import { FaCakeCandles } from 'react-icons/fa6';
 export const Card = {
-    Small : ({text, img, index}) => {
-        const navigate = useNavigate()
-        const moveTo = (index) => {
-            switch(index) {
-                case 0: 
-                    navigate('/search')
-                    console.log('/search')
-                case 1:
-                    navigate('/contract')
-                    console.log('contract')
-                case 2:
-                    navigate('/community/forum')
-                    console.log('forum')
-            }
-        }
+  Small: ({ text, img, index }) => {
+    const navigate = useNavigate();
+    const moveTo = (index) => {
+      switch (index) {
+        case 0:
+          navigate('/search');
+          console.log('/search');
+        case 1:
+          navigate('/contract');
+          console.log('contract');
+        case 2:
+          navigate('/community/forum');
+          console.log('forum');
+      }
+    };
 
-        return(
-            <div className="items-center h-[100px] flex px-8 py-3 rounded-[15px] bg-gray-light drop-shadow-lg gap-2 cursor-pointer" onClick={() => moveTo(index)}>
-                <p className="text-title-small">{text}</p>
-                <img src="public/assets/right circle icon.png"></img>
-                <img src={img}></img>
+    return (
+      <div
+        className="flex h-[100px] cursor-pointer items-center gap-1 rounded-[15px] bg-gray-light px-8 py-3 drop-shadow-lg"
+        onClick={() => moveTo(index)}
+      >
+        <p className="text-nowrap text-title-small">{text}</p>
+        <img src="public/assets/right circle icon.png"></img>
+        <img src={img}></img>
+      </div>
+    );
+  },
+  Medium: () => {},
+  Large: ({ index, name, color }) => {
+    const colorVarient = {
+      gold: 'bg-gold',
+      silver: 'bg-gray-light-medium',
+      bronze: 'bg-light-bronze',
+    };
+
+    return (
+      <div
+        className={`flex h-[304px] w-[303px] flex-col gap-5 px-5 py-3 ${colorVarient[color]} rounded-[15px] drop-shadow-lg`}
+      >
+        <div className="flex items-center gap-3">
+          <img src={`assets/icon-ranking${index + 1}.png`} />
+          <p className="text-title-small">{name}</p>
+          <p className="text-text-small">간병인님</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <FaCircleUser className="h-[112px] w-[112px]" color="#969696" />
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center gap-6">
+              <div className="flex items-center gap-1">
+                <FaCakeCandles color="#969696" />
+                <p>나이</p>
+              </div>
+              <p>50세</p>
             </div>
-        )
-    },
-    Medium : () => {
-
-    },
-    Large : ({index, name, color}) => {
-        
-        const colorVarient = {
-            gold : 'bg-gold',
-            silver : 'bg-gray-light-medium',
-            bronze : 'bg-light-bronze'
-        }
-
-        return(
-            <div className={`w-[303px] h-[304px] flex flex-col gap-5 px-5 py-3 ${colorVarient[color]} rounded-[15px] drop-shadow-lg`}>
-                <div className="flex items-center gap-3">
-                    <img src={`assets/icon-ranking${index+1}.png`}/>
-                    <p className="text-title-small">{name}</p>
-                    <p className="text-text-small">간병인님</p>
-                </div>
-                <div className="flex items-center justify-between">
-                    <FaCircleUser className="w-[112px] h-[112px] " color="#969696"/>
-                    <div className="flex flex-col gap-3">
-                        <div className="flex items-center gap-6">
-                            <div className="flex items-center gap-1">
-                                <FaCakeCandles color="#969696"/>
-                                <p>나이</p>
-                            </div>
-                            <p>50세</p>
-                        </div>
-                        <div className="flex items-center gap-6">
-                            <div className="flex items-center gap-1">
-                                <FaCakeCandles color="#969696"/>
-                                <p>성별</p>
-                            </div>
-                            <p>여성</p>
-                        </div>
-                        <div className="flex items-center gap-6">
-                            <div className="flex items-center gap-1">
-                                <FaCakeCandles color="#969696"/>
-                                <p>경력</p>
-                            </div>
-                            <p>2년</p>
-                        </div>
-                    </div>
-                </div>
-                <hr/>
-                    <div className="flex flex-col gap-3 px-1 py-3">
-                        <div className="flex w-[150px] justify-between">
-                            <p>자격증</p>    
-                            <p>요양보호사</p>
-                        </div>
-                        <div className="flex w-[150px] justify-between">
-                            <p>지역</p>    
-                            <p>서울 강남구</p>
-                        </div>
-                    </div>
+            <div className="flex items-center gap-6">
+              <div className="flex items-center gap-1">
+                <FaCakeCandles color="#969696" />
+                <p>성별</p>
+              </div>
+              <p>여성</p>
             </div>
-        )
-        
-    }
-}
+            <div className="flex items-center gap-6">
+              <div className="flex items-center gap-1">
+                <FaCakeCandles color="#969696" />
+                <p>경력</p>
+              </div>
+              <p>2년</p>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <div className="flex flex-col gap-3 px-1 py-3">
+          <div className="flex w-[150px] justify-between">
+            <p>자격증</p>
+            <p>요양보호사</p>
+          </div>
+          <div className="flex w-[150px] justify-between">
+            <p>지역</p>
+            <p>서울 강남구</p>
+          </div>
+        </div>
+      </div>
+    );
+  },
+};
--- src/components/Home/MainTitleIndex.tsx
+++ src/components/Home/MainTitleIndex.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const MainTitleIndex = ({text, children }) => {
+const MainTitleIndex = ({text, size, children }) => {
+  const sizeVarient = {
+    small : 'text-title-small',
+    medium : 'text-title-medium',
+    large : 'text-title-large'
+  }
+
   return (
     <div className='relative flex flex-col w-full gap-4'>
-      <span className='text-title-medium'>{text}</span>
+      <span className={`${sizeVarient[size]}`}>{text}</span>
       {children}
     </div>
   )
