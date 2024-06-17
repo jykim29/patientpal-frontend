a79ea7a14004a998d21ec722c637d4e26114d952
jykim29 <skt13181@naver.com>
2024-06-18 00:39:24 +0900

    [remove] : 불필요한 주석 제거

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,47 +1,13 @@
-import { useCallback, useEffect, useState } from 'react';
-
-// export type Validator<T> = {
-//   [Key in keyof T]?: {
-//     regex: string;
-//     message: string;
-//   };
-// };
-// type ErrorObject<T> = {
-//   [Key in keyof T]?: { isValid: boolean; message: string } | null;
-// };
+import { useCallback, useState } from 'react';
 
 type Validate<T> = (values: T) => Map<any, any>;
 
-/*
-  TODO
-  1. 한계점 : regex가 단순히 입력값에 대해 정규식 검증만 지원함
-    - 해결해야할 점 : 함수도 지원하여 다양한 검증 방법에 대응
-    (예를들어, 두 개의 값 일치 여부, 다수의 값 true 여부 등... )
-*/
 export function useForm<T extends { [key: string]: any }>(
   formData: T,
   validate: Validate<T>
-  // validator?: Validator<T>
 ) {
   const [data, setData] = useState<T>(formData);
   const [error, setError] = useState<ReturnType<Validate<T>>>(new Map());
-
-  // const validate = () => {
-  //   if (!validator) return;
-  //   const newErrorObject = {} as any;
-  //   const keyValueArray = Object.entries(data as object);
-  //   keyValueArray.forEach(([key, value]) => {
-  //     if (value.length === 0) return;
-  //     const regex = new RegExp(validator[key]?.regex as string);
-  //     if (!regex.test(value))
-  //       newErrorObject[key] = {
-  //         isValid: false,
-  //         message: validator[key]?.message as string,
-  //       };
-  //     // else delete newErrorObject[key as keyof T];
-  //   });
-  //   setError(newErrorObject);
-  // };
 
   const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     const { type, name, value, checked } = e.currentTarget;
@@ -62,10 +28,6 @@ export function useForm<T extends { [key: string]: any }>(
     },
     [data]
   );
-
-  useEffect(() => {
-    // validator && validate();
-  }, [data]);
 
   return { formData: data, handler: { onChange, onSubmit }, error };
 }
