0a715ae565b8d7596e7a9a813209c788118fb5dd
jykim29 <skt13181@naver.com>
2024-06-27 03:51:09 +0900

    [feat] : 액세스 토큰 전역상태관리 함수 구현

---
+++ src/store/useAccessTokenStore.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+type InitialState = { access_token: string | null };
+
+interface TokenState extends InitialState {
+  get: () => void;
+  set: (newToken: string) => void;
+  delete: () => void;
+}
+
+const initialState: InitialState = {
+  access_token: null,
+};
+
+export const useAccessTokenStore = create<TokenState>()(
+  devtools((set) => ({
+    ...initialState,
+    get: () => set((state) => state),
+    set: (newToken) => set(() => ({ access_token: newToken })),
+    delete: () => set(() => ({ access_token: null })),
+  }))
+);
