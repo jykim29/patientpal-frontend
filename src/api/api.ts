7433c1d21d044718ab215724765589611f9a4aea
jykim29 <skt13181@naver.com>
2024-06-27 03:49:05 +0900

    [feat] : http로 원상복구 및 서버 프록시 추가
    - vite-plugin-mkcert 플러그인 적용 해제
    - 서버 프록시 우회 기능 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API_BASE_URL,
+  // baseURL: import.meta.env.VITE_BACKEND_API_BASE_URL,
+  baseURL: '/api/v1',
   timeout: 1000,
   headers: { 'Content-Type': 'application/json' },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import * as path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
-import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [react(), svgrPlugin(), mkcert()],
+  plugins: [react(), svgrPlugin()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://43.201.129.48:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   resolve: {
     alias: {
