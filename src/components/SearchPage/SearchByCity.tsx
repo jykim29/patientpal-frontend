4ceac31d8577266c4f4f791f41000a002d33caf4
shgusgh12 <https://github.com/shgusgh12>
2024-06-27 14:15:32 +0900

    [refactor] : 필요없는 태그 삭제

--- src/components/SearchPage/SearchByCity.tsx
+++ src/components/SearchPage/SearchByCity.tsx
@@ -4,14 +4,12 @@ import SearchForm from './SearchForm';
 
 function SearchByCity() {
   return (
-    <div className="">
-      <MainTitleIndex size="small" text="지역별 찾기">
-        <div className="flex h-[565px] w-full rounded-[15px] border-[1.5px] shadow-lg">
-          <img src="/assets/map.png" className="w-[696px]" />
-          <SearchForm></SearchForm>
-        </div>
-      </MainTitleIndex>
-    </div>
+    <MainTitleIndex size="small" text="지역별 찾기">
+      <div className="flex h-[565px] w-full rounded-[15px] border-[1.5px] shadow-lg">
+        <img src="/assets/map.png" className="w-[696px]" />
+        <SearchForm />
+      </div>
+    </MainTitleIndex>
   );
 }
 
