25c366a753b81693a69ecce1062e43ec37e8aeb4
shgusgh12 <https://github.com/shgusgh12>
2024-06-27 14:16:57 +0900

    [refactor] : 검색 페이지 route 수정

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -3,15 +3,21 @@ import MenuTitle from '../components/common/MenuTitle';
 import SearchResult from '../components/SearchPage/SearchResult';
 import SearchByCity from '../components/SearchPage/SearchByCity';
 import SearchByMap from '../components/SearchPage/SearchByMap';
-import { useLocation } from 'react-router-dom';
 
-function SearchPage() {
-  const location = useLocation();
+type SearchType = 'map' | 'city';
+
+interface Props {
+  searchType: SearchType;
+}
+
+function SearchPage({ searchType }: Props) {
+  //submit을 props로 전달하기 SearchByCity, SearchByMap에다가
+
   return (
     <main className="flex flex-col justify-center gap-[52px] px-[52px] py-8">
       <MenuTitle title="간병인 찾기" />
-      {location.pathname === '/search/city' && <SearchByCity />}
-      {location.pathname === '/search/map' && <SearchByMap />}
+      {searchType === 'city' && <SearchByCity />}
+      {searchType === 'map' && <SearchByMap />}
       <SearchResult />
     </main>
   );
