303156b702a2230afc15f66fff89a8547f1b4882
jykim29 <skt13181@naver.com>
2024-06-27 14:50:13 +0900

    [remove] : axios 인스턴스 파일 변경으로 인한 이전 파일 삭제

--- src/api/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  // baseURL: import.meta.env.VITE_BACKEND_API_BASE_URL,
-  baseURL: '/api/v1',
-  timeout: 1000,
-  headers: { 'Content-Type': 'application/json' },
-});
