527b0e8899b0dd9cd2b6b5b2786cb9cf7d97640a
jykim29 <skt13181@naver.com>
2024-06-29 17:16:08 +0900

    [refactor] : vite config 및 axios 인스턴스 리팩토링

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,20 +1,24 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import { API_BASE_URL, PROXY_BASE_URL, TIMEOUT_SECONDS } from '@/constants/api';
 
-const BASE_URL = '/api/v1';
+const BASE_URL =
+  import.meta.env.MODE === 'development' ? PROXY_BASE_URL : API_BASE_URL;
 
-export const createClient = (config?: AxiosRequestConfig) => {
+const createAxiosInstance = (
+  baseURL: string,
+  defaultConfig: AxiosRequestConfig = {}
+) => {
   const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    timeout: 30000,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: '', //토큰 처리
-    },
-    withCredentials: true,
-    ...config,
+    baseURL,
+    ...defaultConfig,
   });
-
   return axiosInstance;
 };
 
-export const httpClient = createClient();
+export const httpClient = createAxiosInstance(BASE_URL, {
+  timeout: 1 * 1000 * TIMEOUT_SECONDS,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,27 @@
 import * as path from 'path';
 
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgrPlugin from 'vite-plugin-svgr';
 
-export default defineConfig({
-  plugins: [react(), svgrPlugin()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://43.201.129.48:8080',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react(), svgrPlugin()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  };
 });
