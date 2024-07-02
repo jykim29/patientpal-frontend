ed665d8beb171f2b969b4b5d4cc4dd7ecfeda1ec
jykim29 <skt13181@naver.com>
2024-07-02 19:02:36 +0900

    [feat] : AuthService 클래스 생성으로 비즈니스 로직 분리
    - 컴포넌트에 종속되어있던 인증 관련 비즈니스 로직들을 분리하여 클래스로 통합하여 인스턴스 생성

---
+++ src/services/AuthService.ts
@@ -0,0 +1,43 @@
+import authApi from '@/api/auth/authApi';
+import { FETCH_FAILED, FETCH_SUCCESS } from '@/constants/api';
+import { useAccessTokenStore } from '@/store/useAccessTokenStore';
+import { AuthApi, RequestBody } from '@/types/authApi.type';
+
+class AuthService {
+  private authApi: ReturnType<AuthApi>;
+  private setToken;
+  constructor(api: ReturnType<AuthApi>) {
+    this.authApi = api;
+    this.setToken = useAccessTokenStore.getState().set;
+  }
+  public async signInWithIdPassword(
+    formData: RequestBody['login'],
+    config = {}
+  ) {
+    const { data, status } = await this.authApi.login(formData, config);
+    if (status === FETCH_FAILED) {
+      return { message: data.message as string, status: FETCH_FAILED };
+    }
+    this.setToken(data.access_token);
+    return { message: null, status: FETCH_SUCCESS };
+  }
+
+  public async signUp(formData: RequestBody['register'], config = {}) {
+    const { data, status } = await this.authApi.register(formData, config);
+    if (status === FETCH_FAILED) {
+      return { message: data.message as string, status: FETCH_FAILED };
+    }
+    return { message: null, status: FETCH_SUCCESS };
+  }
+
+  public async refreshToken(config = {}) {
+    const { data, status } = await this.authApi.refresh(config);
+    if (status === FETCH_FAILED) {
+      return { message: data.message as string, status: FETCH_FAILED };
+    }
+    this.setToken(data.access_token);
+    return { message: null, status: FETCH_SUCCESS };
+  }
+}
+
+export const authService = new AuthService(authApi);
