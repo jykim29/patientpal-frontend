a899eb3db737b37221e562d564071ee861d0af67
jykim29 <skt13181@naver.com>
2024-07-29 17:08:33 +0900

    [refactor] : 사용자 인증 상태관리 함수 리팩토링
    - 사용자 데이터 받는 API 함수 작성
    - useAuthStore 상태관리에 추가
    - 프로필이 필요한 페이지에 접근 차단 로직 작성
    - 기타 코드 리팩토링

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -3,11 +3,14 @@ import { AxiosRequestConfig } from 'axios';
 import { HTTPClient, httpClient } from '@/api/httpClient';
 import { API_ENDPOINT, API_FAILED, API_SUCCESS } from '@/constants/api';
 import {
+  GetUserDataResponse,
   RefreshTokenResponse,
   RequestBody,
   SignInResponse,
   SignUpResponse,
 } from '@/types/api/auth';
+import { FetchResult } from '@/types/api/common';
+import { decodeTokenPayload } from '@/utils/decodeTokenPayload';
 import { useAuthStore } from './../store/useAuthStore';
 
 class AuthService {
@@ -22,56 +25,70 @@ class AuthService {
   async signInWithIdPassword(
     formData: RequestBody['login'],
     config: AxiosRequestConfig = {}
-  ) {
+  ): Promise<FetchResult<SignInResponse>> {
+    // 로그인 API
     const { data, status } = await this.httpClient.POST<
       RequestBody['login'],
       SignInResponse
     >(API_ENDPOINT.AUTH.LOGIN, formData, config);
     if (status === API_FAILED) {
-      return { message: data.message as string, status: API_FAILED };
+      return { data, status: API_FAILED };
     }
-    this.updateData({
-      isLoggedIn: true,
-      accessToken: data.access_token,
-      lastLogin: new Date().toISOString(),
-      user: {},
-    });
-    return { message: null, status: API_SUCCESS };
+    return { data: data, status: API_SUCCESS };
   }
 
   async signUp(
     formData: RequestBody['register'],
     config: AxiosRequestConfig = {}
-  ) {
+  ): Promise<FetchResult<SignUpResponse>> {
     const { data, status } = await this.httpClient.POST<
       RequestBody['register'],
       SignUpResponse
     >(API_ENDPOINT.AUTH.REGISTER, formData, config);
     if (status === API_FAILED) {
-      return { message: data.message as string, status: API_FAILED };
+      return { data, status: API_FAILED };
     }
     return {
-      message: '회원가입이 완료되었습니다. 로그인페이지로 이동합니다.',
+      data: {
+        message: '회원가입이 완료되었습니다. 로그인페이지로 이동합니다.',
+      },
       status: API_SUCCESS,
     };
   }
 
-  async refreshToken(config: AxiosRequestConfig = {}) {
+  async refreshToken(
+    config: AxiosRequestConfig = {}
+  ): Promise<FetchResult<RefreshTokenResponse>> {
     const { data, status } = await this.httpClient.POST<
       null,
       RefreshTokenResponse
     >(API_ENDPOINT.AUTH.REFRESH, null, config);
     if (status === API_FAILED) {
       this.resetData();
-      return { message: data.message as string, status: API_FAILED };
+      return { data, status: API_FAILED };
     }
+    return { data, status: API_SUCCESS };
+  }
+
+  async getUserData(
+    token: string,
+    config: AxiosRequestConfig = {}
+  ): Promise<FetchResult<GetUserDataResponse>> {
+    const { data, status } = await this.httpClient.GET<GetUserDataResponse>(
+      API_ENDPOINT.MEMBER.INFORMATION,
+      { baseURL: '/api', ...config }
+    );
+    if (status === API_FAILED) return { data, status: API_FAILED };
     this.updateData({
       isLoggedIn: true,
-      accessToken: data.access_token,
+      accessToken: token,
       lastLogin: new Date().toISOString(),
-      user: {},
+      user: {
+        ...data,
+        role: decodeTokenPayload(token).auth,
+      },
     });
-    return { message: null, status: API_SUCCESS };
+    return { data, status: API_SUCCESS };
   }
 }
 
