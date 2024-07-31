899438adf23ed9ad86957a3bb66bd59bfea7dc81
jykim29 <skt13181@naver.com>
2024-08-01 02:16:51 +0900

    [design] : BoardArticle 컴포넌트 css파일 생성 및 적용

--- src/components/board/BoardArticle.tsx
+++ src/components/board/BoardArticle.tsx
@@ -12,12 +12,16 @@ import Button from '../common/Button';
 export default function BoardArticle() {
   const { title, content, name, id, createdAt, updatedAt, memberId, postType } =
     useLoaderData() as PostResponse;
-  const { accessToken } = useAuthStore();
+  const { accessToken, user } = useAuthStore();
   const navigate = useNavigate();
+  const boardType = postType === 'FREE' ? 'board' : 'notice';
+  const myId = user && user.memberId;
+  const isMyPost = myId === memberId;
 
   const handleDeletePost = async () => {
     if (!confirm('정말 삭제하시겠습니까?')) return;
-    const response = await boardService.deletePost('FREE', id, {
+    if (!isMyPost) return;
+    const response = await boardService.deletePost(boardType, id, {
       headers: { Authorization: `Bearer ${accessToken}` },
     });
     if (response?.status === API_FAILED) return alert('통신 실패');
@@ -25,13 +29,11 @@ export default function BoardArticle() {
   };
   return (
     <>
-      <div className="mt-3 flex w-full flex-col overflow-hidden rounded-md border border-gray-medium bg-white">
-        <div className="border-b border-gray-medium bg-gray-light px-5 py-3">
-          <h3 className="mb-2 text-text-large font-semibold">{title}</h3>
-          <span className="relative pr-3 after:absolute after:-right-[2px] after:top-1/2 after:-translate-y-1/2 after:text-text-small after:text-gray-medium after:content-['|']">
-            {name}
-          </span>
-          <span className="relative px-3 after:absolute after:-right-[2px] after:top-1/2 after:-translate-y-1/2 after:text-text-small after:text-gray-medium after:content-['|']">
+      <div className="article-container">
+        <div className="article-header">
+          <h3 className="article-title">{title}</h3>
+          <span className="divider pr-3">{name}</span>
+          <span className="divider px-3">
             <span className="mr-2">작성일</span>
             <time className="text-gray-medium-dark" dateTime={createdAt}>
               {`${convertDatetime(createdAt)[0]} ${convertDatetime(createdAt)[1]}`}
@@ -51,24 +53,22 @@ export default function BoardArticle() {
         ></div>
       </div>
 
-      <div className="mt-3 text-right">
-        <Link
-          to={`../modify/${id}`}
-          className="mr-2 inline-block h-8 rounded-md bg-gray-medium px-4 py-1 align-top text-text-medium text-white transition-all hover:brightness-[0.95] active:brightness-[1.05]"
-        >
-          수정
-        </Link>
-        <Button
-          type="button"
-          className="mr-2 h-8 bg-negative px-4 py-1"
-          onClick={handleDeletePost}
-        >
-          삭제
-        </Button>
-        <Link
-          to={'../'}
-          className="mr-2 inline-block h-8 rounded-md bg-primary px-4 py-1 align-top text-text-medium text-white transition-all hover:brightness-[0.95] active:brightness-[1.05]"
-        >
+      <div className="article-button-container">
+        {isMyPost && (
+          <>
+            <Link to={`../modify/${id}`} className="link-button bg-gray-medium">
+              수정
+            </Link>
+            <Button
+              type="button"
+              className="mr-2 h-8 bg-negative px-4 py-1"
+              onClick={handleDeletePost}
+            >
+              삭제
+            </Button>
+          </>
+        )}
+        <Link to={'../'} className="link-button bg-primary">
           목록으로
         </Link>
       </div>
