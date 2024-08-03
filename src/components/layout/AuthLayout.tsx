628b6bf57eee04ce41395492a94b7c22361a20ff
jykim29 <skt13181@naver.com>
2024-08-04 01:01:10 +0900

    [design] : 레이아웃 및 푸터 컴포넌트 스타일 변경

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -3,9 +3,11 @@ import Header from '../common/Header';
 
 function AuthLayout() {
   return (
-    <div className="flex h-screen w-screen flex-col justify-between">
-      <Header />
-      <main className="flex w-full flex-1 items-center justify-center bg-gray-light-medium py-2">
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <div className="w-full">
+        <Header />
+      </div>
+      <main className="mx-auto flex w-full min-w-[1440px] flex-1 items-center justify-center bg-gray-light-medium py-2">
         <section className="flex h-[650px] w-[800px] justify-between overflow-hidden rounded-3xl bg-white shadow-md">
           <Outlet />
           <div className="flex-1 bg-secondary"></div>
