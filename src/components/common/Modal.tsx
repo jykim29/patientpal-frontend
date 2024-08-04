3b233cc6535b61affe799a336799b21562c1b4d6
jykim29 <skt13181@naver.com>
2024-08-04 16:04:22 +0900

    [comment] : Modal 컴포넌트 우상단 'X' 닫기 버튼 임시 주석 처리
    - 재사용성을 고려했을 때, 우상단의 'X' 닫기 버튼의 효용성 문제로 임시 주석 처리

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -57,21 +57,21 @@ function ModalBox({
   className,
   children,
 }: PropsWithChildren<{ className?: string }>) {
-  const { closeAllModal } = useModal();
+  // const { closeAllModal } = useModal();
   const combinedClassName = twMerge(
     'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl bg-white overflow-hidden',
     className
   );
   return (
     <div className={combinedClassName}>
-      <button
-        className="absolute right-3 top-3 ml-auto h-6 w-6 bg-[url('/assets/cross_black.svg')] bg-center bg-no-repeat p-4 hover:bg-gray-light"
+      {/* <button
+        className="absolute right-3 top-3 ml-auto h-4 w-4 bg-[url('/assets/cross_black.svg')] bg-center bg-no-repeat hover:bg-gray-light"
         type="button"
         title="창 닫기"
         onClick={() => closeAllModal()}
       >
         <span className="sr-only">창 닫기</span>
-      </button>
+      </button> */}
       {children}
     </div>
   );
