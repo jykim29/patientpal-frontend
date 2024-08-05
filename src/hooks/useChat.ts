c374b91ff5e51e6de2585a2170b35b046030613e
jykim29 <skt13181@naver.com>
2024-08-05 17:35:21 +0900

    [refactor] : 동작 방식 변경을 위한 코드 리팩토링
    - 웹소켓 연결 및 연결종료시 자동으로 채팅방에 입장 또는 퇴장하도록 변경

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -1,37 +1,45 @@
 import { Client } from '@stomp/stompjs';
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export function useChat(stompClient: Client) {
   const [isConnected, setIsConnected] = useState<boolean>(false);
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<{ [key: string]: any }[]>([]);
+  const { roomId } = useParams();
+  const chatRoomId = roomId as string;
 
-  const createChatRoom = (roomId: string) => {
-    const join = () => {
-      stompClient.subscribe(
-        `/topic/directChat/${roomId}`,
-        (message) => {
-          console.log(message);
-          setMessages((prev) => [...prev, JSON.parse(message.body)]);
-        },
-        { id: roomId }
-      );
-    };
-    const leave = () => {
-      stompClient.unsubscribe(roomId);
-    };
-    const send = (message: string) => {
-      stompClient.publish({
-        destination: `/app/chat/${roomId}`,
-        body: JSON.stringify({ content: message }),
-      });
-    };
-    return { join, leave, send };
+  const joinRoom = (roomId: string) => {
+    console.log('join room');
+    stompClient.subscribe(
+      `/topic/directChat/${roomId}`,
+      (message) => {
+        console.log('메세지', message);
+        setMessages((prev) => [...prev, JSON.parse(message.body)]);
+      },
+      { id: roomId }
+    );
+  };
+  const leaveRoom = (roomId: string) => {
+    console.log('leave room');
+    stompClient.unsubscribe(roomId);
+  };
+  const sendMessage = (message: string) => {
+    stompClient.publish({
+      destination: `/app/chat/${chatRoomId}`,
+      body: JSON.stringify({ content: message }),
+    });
   };
 
   useEffect(() => {
     stompClient.onConnect = (frame) => {
       console.log('연결됨', frame);
+      joinRoom(chatRoomId);
       setIsConnected(true);
+    };
+    stompClient.onDisconnect = (frame) => {
+      console.log('연결끊김', frame);
+      leaveRoom(chatRoomId);
+      setIsConnected(false);
     };
     stompClient.activate();
     return () => {
@@ -40,5 +48,5 @@ export function useChat(stompClient: Client) {
     };
   }, []);
 
-  return { isConnected, createChatRoom, messages };
+  return { isConnected, joinRoom, leaveRoom, sendMessage, messages };
 }
