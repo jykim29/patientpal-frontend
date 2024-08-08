fe193ee96cd34be5e2db31af6c0eae933bb122c5
jykim29 <skt13181@naver.com>
2024-08-09 02:23:26 +0900

    [refactor] : Fragment key prop 추가 및 textarea 스타일 변경

--- src/components/Modal/ReviewWriteModal.tsx
+++ src/components/Modal/ReviewWriteModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import Button from '../common/Button';
 import { FormAlertErrorBox } from '../Form';
 
@@ -31,7 +31,7 @@ export default function ReviewWriteModal() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setErrorMessage(null);
-    if (formData.star <= 0 || formData.star > 5)
+    if (formData.star < 1 || formData.star > 5)
       return setErrorMessage('평점은 1점부터 5점까지 가능합니다.');
     if (formData.content.trim().length === 0)
       return setErrorMessage('후기 내용을 입력해주세요.');
@@ -57,7 +57,7 @@ export default function ReviewWriteModal() {
                 ? "bg-[url('/assets/review_star_empty.png')]"
                 : "bg-[url('/assets/review_star_full.png')]";
             return (
-              <>
+              <Fragment key={value}>
                 <label
                   className={`${starImage} cursor-pointer rounded-full bg-[length:45px_45px] bg-center bg-no-repeat p-7 transition-all duration-[50ms] hover:scale-105 hover:bg-gray-light`}
                   htmlFor={value}
@@ -72,7 +72,7 @@ export default function ReviewWriteModal() {
                   value={value}
                   onChange={handleChange}
                 />
-              </>
+              </Fragment>
             );
           })}
         </div>
@@ -90,7 +90,7 @@ export default function ReviewWriteModal() {
           후기 내용
         </label>
         <textarea
-          className="h-24 w-full resize-none rounded-md border-2 border-gray-medium p-3 outline-none focus-visible:border-secondary"
+          className="h-24 w-full resize-none rounded-md border-2 border-gray-medium p-3 focus:outline-none focus-visible:border-secondary"
           name="content"
           id="content"
           placeholder="후기 내용을 입력해주세요."
