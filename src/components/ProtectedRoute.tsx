74a1c9a00fe2ee93e635e8a5ad2ac5c025d1a1bd
jykim29 <skt13181@naver.com>
2024-08-09 18:41:53 +0900

    [refactor] : 기존 FeedbackModal 컴포넌트를 커스텀 alert 및 confirm 으로 대체

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -3,7 +3,6 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { useAuthStore } from '@/store/useAuthStore';
 import { useModal } from '@/hooks/useModal';
-import { FeedbackModal } from './Modal';
 
 const unneedProfilePathArray: string[] = [
   '/mypage/profile',
@@ -12,7 +11,7 @@ const unneedProfilePathArray: string[] = [
 
 export default function ProtectedRoute() {
   const { isLoggedIn, user } = useAuthStore();
-  const { createModal, openModal } = useModal();
+  const { alert } = useModal();
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
@@ -22,43 +21,25 @@ export default function ProtectedRoute() {
       user.isCompleteProfile === false &&
       pathname.includes(value) === false
   );
-  const authRequiredModal = createModal(
-    { modalName: 'auth-required', closeOnOverlayClick: true },
-    <FeedbackModal
-      iconType="warning"
-      buttonType="confirm"
-      onConfirm={() => navigate('/auth/signin', { replace: true })}
-    >
-      <p className="break-words text-center font-semibold">
-        로그인이 필요한 페이지입니다.
-        <br />
-        로그인 페이지로 이동합니다.
-      </p>
-    </FeedbackModal>
-  );
-  const profileRequiredModal = createModal(
-    { modalName: 'profile-required', closeOnOverlayClick: true },
-    <FeedbackModal
-      iconType="warning"
-      buttonType="confirm"
-      onConfirm={() => navigate('/mypage/profile', { replace: true })}
-    >
-      <p className="break-words text-center font-semibold">
-        서비스 이용을 위해서 먼저 프로필 작성이 필요합니다.
-      </p>
-    </FeedbackModal>
-  );
 
   useEffect(() => {
-    if (!user && !isLoggedIn) openModal('auth-required');
-    if (isNeedProfilePath) openModal('profile-required');
+    if (!user && !isLoggedIn) {
+      alert(
+        'warning',
+        '로그인이 필요한 페이지입니다. 로그인 페이지로 이동합니다.'
+      ).then((res) => {
+        if (res) navigate('/auth/signin', { replace: true });
+      });
+    }
+    if (isNeedProfilePath) {
+      alert(
+        'warning',
+        '서비스 이용을 위해서 먼저 프로필 작성이 필요합니다.'
+      ).then((res) => {
+        if (res) navigate('/mypage/profile', { replace: true });
+      });
+    }
   }, [pathname]);
 
-  return (
-    <>
-      {isLoggedIn && !isNeedProfilePath && <Outlet />}
-      {authRequiredModal}
-      {profileRequiredModal}
-    </>
-  );
+  return <>{isLoggedIn && !isNeedProfilePath && <Outlet />}</>;
 }
