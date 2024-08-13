f7a2a8ac3eda338ec7e13d6753a3336b126ed686
jykim29 <skt13181@naver.com>
2024-08-13 16:50:36 +0900

    [rename] : ChatRoomCard 에서 ChatRoomListItem 으로 파일명 변경

--- src/components/Chat/ChatRoomListItem.tsx
+++ src/components/Chat/ChatRoomListItem.tsx
@@ -1,25 +1,21 @@
 import { Link } from 'react-router-dom';
 
 interface ChatRoomCardProps {
-  roomId: string;
+  roomId: number;
   username: string;
   lastMessage: string;
   lastReceiveDate: string;
-  unreadMessageCount: string;
   profileImageUrl?: string;
 }
 
-export default function ChatRoomCard({
+export default function ChatRoomListItem({
   roomId,
   username,
   lastMessage,
   lastReceiveDate,
-  unreadMessageCount,
-  profileImageUrl = '/assets/default_profile_woman.png',
+  profileImageUrl = '/assets/default_profile.jpg',
 }: ChatRoomCardProps) {
   const date = lastReceiveDate.slice(0, 10);
-  const messageCount =
-    Number(unreadMessageCount) > 100 ? '100+' : unreadMessageCount;
   return (
     <Link
       className="rounded-2xl border border-tertiary bg-white p-10 px-7 py-6 shadow-[0px_0px_8px_4px_#a2bffe40] transition-all hover:bg-blue-100"
@@ -39,9 +35,6 @@ export default function ChatRoomCard({
 
         <div className="ml-auto flex flex-col items-center justify-center">
           <time dateTime={new Date().toISOString()}>{date}</time>
-          <span className="rounded-full bg-negative px-1.5 font-semibold text-white">
-            {messageCount}
-          </span>
         </div>
       </li>
     </Link>
