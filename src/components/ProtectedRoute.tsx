253b5abd48f758a9badc97deff60bb48ecef4711
jykim29 <skt13181@naver.com>
2024-08-15 00:37:40 +0900

    [refactor] : 전체적으로 접근 권한 재설정
    - 검색 및 커뮤니티 페이지는 사용자 비인증 상태에서도 접근 가능하도록 변경
    - 단, 비인증 상태에서 커뮤니티 게시판의 열람 범위는 공지사항으로 제한함
    - 마이페이지 내의 메뉴들은 기존대로 사용자 인증 및 프로필이 작성되어야 이용가능하도록 유지

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -4,42 +4,53 @@ import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '@/store/useAuthStore';
 import { useModal } from '@/hooks/useModal';
 
-const unneedProfilePathArray: string[] = [
-  '/mypage/profile',
+const loginBypassList = [
+  '/search/city',
+  '/search/map',
   '/community/notice',
+  '/community/notice/view/:postId',
+  '/community/forum',
 ];
+const profileBypassList = ['/mypage/profile', '/community/forum/view/:postId'];
+
+const matchExactPathname = (pathname: string, currentPathname: string) => {
+  const regexPath = pathname.replace(/:\w+/g, '[^/]+');
+  const regex = new RegExp(`^${regexPath}$`);
+  return regex.test(currentPathname);
+};
 
 export default function ProtectedRoute() {
   const { isLoggedIn, user } = useAuthStore();
   const { alert } = useModal();
   const navigate = useNavigate();
   const { pathname } = useLocation();
-
-  const isNeedProfilePath = unneedProfilePathArray.every(
-    (value) =>
-      user &&
-      user.isCompleteProfile === false &&
-      pathname.includes(value) === false
+  const isLoginBypass = loginBypassList.some((value) =>
+    matchExactPathname(value, pathname)
   );
+  const isProfileBypass = profileBypassList.some((value) =>
+    matchExactPathname(value, pathname)
+  );
+  const isHideOutlet =
+    !isLoginBypass &&
+    (!isLoggedIn ||
+      (!isProfileBypass && isLoggedIn && !user?.isCompleteProfile));
 
   useEffect(() => {
-    if (!user && !isLoggedIn) {
-      alert(
-        'warning',
-        '로그인이 필요한 페이지입니다. 로그인 페이지로 이동합니다.'
-      ).then((res) => {
-        if (res) navigate('/auth/signin', { replace: true });
-      });
-    }
-    if (isNeedProfilePath) {
-      alert(
-        'warning',
-        '서비스 이용을 위해서 먼저 프로필 작성이 필요합니다.'
-      ).then((res) => {
-        if (res) navigate('/mypage/profile', { replace: true });
-      });
+    if (!isLoginBypass) {
+      // 로그인 필요 페이지인데 로그인 안했을 경우
+      if (!isLoggedIn)
+        alert(
+          'warning',
+          '로그인이 필요한 페이지입니다. 로그인 페이지로 이동합니다.'
+        ).then(() => navigate('/auth/signin'));
+      // 로그인 및 프로필 필요 페이지인데 로그인 했는데 프로필 없을 경우
+      if (isLoggedIn && !isProfileBypass && !user?.isCompleteProfile)
+        alert(
+          'warning',
+          '서비스 이용을 위해 먼저 프로필 작성이 필요합니다. '
+        ).then(() => navigate('/mypage/profile'));
     }
   }, [pathname]);
 
-  return <>{isLoggedIn && !isNeedProfilePath && <Outlet />}</>;
+  return <>{isHideOutlet ? null : <Outlet />}</>;
 }
